ca8b85ba295a015ce19d676157963363447f400a
halinh2220 <154485067+halinh2220@users.noreply.github.com>
2023-12-29 04:00:21 +0700

    up

---
+++ Assets/Javascript/js.js
@@ -0,0 +1,36 @@
+function momodal1(){
+    var query = document.querySelector("#homepage-content .second-content .second-content-right .uiux-main .hidden-content");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
+
+function momodal2(){
+    var query = document.querySelector("#homepage-content .second-content .second-content-right .branding-main .hidden-content");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
+
+function momodal3(){
+    var query = document.querySelector("#homepage-content .second-content .second-content-right .animation-main .hidden-content");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
+
+function momodal4(){
+    var query = document.querySelector("#homepage-content .second-content .second-content-right .illustrator-main .hidden-content");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
+
+function momodal5(){
+    var query = document.querySelector("#homepage-content .second-content .second-content-right .baobi-main .hidden-content");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
+
+
